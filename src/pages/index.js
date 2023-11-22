e0d32eb5d0be88107b0a97e5c4b787a70bda9703
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-22 15:28:16 -0800

    logo colors update, index update & prettier fix width

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 const BlockSpamEarnCash = () => {
   const data = useStaticQuery(graphql`
     query {
-      fyncomFilterGmail: file(
-        relativePath: { eq: "fyncom_filters_gmail_edition_no_logo.png" }
-      ) {
+      fyncomFilterGmail: file(relativePath: { eq: "fyncom_filters_gmail_edition_no_logo.png" }) {
         childImageSharp {
           gatsbyImageData(width: 300, layout: CONSTRAINED, placeholder: BLURRED)
         }
@@ -27,9 +25,7 @@ const BlockSpamEarnCash = () => {
           gatsbyImageData(width: 300, layout: CONSTRAINED, placeholder: BLURRED)
         }
       }
-      fyncomFilterGmailDark: file(
-        relativePath: { eq: "fyncom_filters_gmail_edition_no_logo-white.png" }
-      ) {
+      fyncomFilterGmailDark: file(relativePath: { eq: "fyncom_filters_gmail_edition_no_logo-white.png" }) {
         childImageSharp {
           gatsbyImageData(width: 300, layout: CONSTRAINED, placeholder: BLURRED)
         }
@@ -44,11 +40,7 @@ const BlockSpamEarnCash = () => {
           gatsbyImageData(width: 300, layout: CONSTRAINED, placeholder: BLURRED)
         }
       }
-      increaseResponseRates: file(
-        relativePath: {
-          eq: "increase-response=rates-across-any platform-and-channel.png"
-        }
-      ) {
+      increaseResponseRates: file(relativePath: { eq: "increase-response=rates-across-any platform-and-channel.png" }) {
         childImageSharp {
           gatsbyImageData(width: 300, layout: CONSTRAINED, placeholder: BLURRED)
         }
@@ -105,20 +97,12 @@ const BlockSpamEarnCash = () => {
       }
     }
   `)
-  const filterImage = getImage(
-    data.fyncomFilterGmail.childImageSharp.gatsbyImageData
-  )
-  const filterImageDark = getImage(
-    data.fyncomFilterGmailDark.childImageSharp.gatsbyImageData
-  )
-  const increaseCustomerResponse = getImage(
-    data.increaseResponseRates.childImageSharp.gatsbyImageData
-  )
+  const filterImage = getImage(data.fyncomFilterGmail.childImageSharp.gatsbyImageData)
+  const filterImageDark = getImage(data.fyncomFilterGmailDark.childImageSharp.gatsbyImageData)
+  const increaseCustomerResponse = getImage(data.increaseResponseRates.childImageSharp.gatsbyImageData)
   const tinderLogo = getImage(data.tinderLogo.childImageSharp.gatsbyImageData)
   const discordLogo = getImage(data.discordLogo.childImageSharp.gatsbyImageData)
-  const telegramLogo = getImage(
-    data.telegramLogo.childImageSharp.gatsbyImageData
-  )
+  const telegramLogo = getImage(data.telegramLogo.childImageSharp.gatsbyImageData)
   const karmacallImage = getImage(data.karmaCall.childImageSharp.gatsbyImageData)
   const karmacallImageDark = getImage(data.karmaCallDark.childImageSharp.gatsbyImageData)
   const oneMillionCups = getImage(data.oneMillionCups.childImageSharp.gatsbyImageData)
@@ -127,7 +111,6 @@ const BlockSpamEarnCash = () => {
   const disruptionBankingDark = getImage(data.disruptionBankingDark.childImageSharp.gatsbyImageData)
   const appStoreBadge = getImage(data.appStoreBadge.childImageSharp.gatsbyImageData)
   const googlePlayBadge = getImage(data.googlePlayBadge.childImageSharp.gatsbyImageData)
-
 
   // Use state to keep track of the images for the current theme
   const [filterLogo, setFilterLogo] = useState(filterImage)
@@ -152,83 +135,122 @@ const BlockSpamEarnCash = () => {
       mediaQuery.addListener(handleChange)
       return () => mediaQuery.removeListener(handleChange)
     }
-  }, [karmacallImage, karmacallImageDark, disruptionBanking, disruptionBankingDark,
-    oneMillionCups, oneMillionCupsDark, data.evonexusDark.publicURL, data.evonexus.publicURL])
+  }, [
+    karmacallImage,
+    karmacallImageDark,
+    disruptionBanking,
+    disruptionBankingDark,
+    oneMillionCups,
+    oneMillionCupsDark,
+    data.evonexusDark.publicURL,
+    data.evonexus.publicURL,
+  ])
 
   return (
     <div>
       <Seo title="KarmaCall" />
       <Header />
       <section>
-        <div className="AppText">
-          <div className="social-media-container">
-            <div className="logo-container">
-              <div className="bottom-logo">
-                <img src={data.heroKarmaCallImage.publicURL} className={"hero-index-image"}
-                     alt="A simple app that pays you to block scam calls." />
+        <div className="parent-container">
+          <div className="AppText">
+            <div className="social-media-container">
+              <div className="logo-container">
+                <div className="bottom-logo">
+                  <img src={data.heroKarmaCallImage.publicURL} className={"hero-index-image"} alt="A simple app that pays you to block scam calls." />
+                </div>
+              </div>
+              <div className="text-block">
+                <h1>Get Paid to Block Scam Calls.</h1>
+                <h2>Cash out with no limits</h2>
+                <p>
+                  Hang up on scam phone call and take the caller's deposit! Get immediate financial revenge against scammers who waste your time. Join us in the
+                  fight against malicious phone spammers by downloading KarmaCall today.
+                </p>
               </div>
             </div>
-            <div className="text-block">
-              <h1>Get Paid to Block Scam Calls.</h1>
-              <h2>Cash out with no limits</h2>
-              <p>Hang up on scam phone call and take the caller's deposit!
-                Get immediate financial revenge against scammers who waste your time.
-                Join us in the fight against malicious phone spammers by downloading KarmaCall today.
-              </p>
-            </div>
           </div>
-        </div>
-        <div className={"app-store-row"}>
-              <a
-                href="https://play.google.com/store/apps/details?id=com.fyncom.robocash"
-              >
-                <GatsbyImage
-                  className="app-img-index"
-                  image={googlePlayBadge}
-                  alt="Get KarmaCall on Google Play"
-                />
-              </a>
-              <a
-                href="https://apps.apple.com/us/app/karmacall/id1574524278"
-              >
-                <GatsbyImage
-                  className="app-img-index"
-                  image={appStoreBadge}
-                  alt="Download KarmaCall on the App Store"
-                />
-              </a>
+          <div className={"app-store-row"}>
+            <a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash">
+              <GatsbyImage className="app-img-index" image={googlePlayBadge} alt="Get KarmaCall on Google Play" />
+            </a>
+            <a href="https://apps.apple.com/us/app/karmacall/id1574524278">
+              <GatsbyImage className="app-img-index" image={appStoreBadge} alt="Download KarmaCall on the App Store" />
+            </a>
+          </div>
         </div>
 
         <div className="AppText">
           <div className="social-media-container">
             <div className="text-block-left">
-              <h2 className="text-wrapper-2">
-                Are your customers getting{" "}
-                <span className="underline-red">phished</span>?
+              <h2 className="text-wrapper-5">
+                Your Refundable Paywall. <br />
+                Your <span className="underline-karma">Karma</span>Call.
+              </h2>
+              <div className={"values-column"}>
+                <div className="value-row">
+                  <div className="icon-container">
+                    {/* <div className="logo-container"> */}
+                    <FaDollarSign />
+                  </div>
+                  <div className="text-container">
+                    <h4>Get Paid!</h4>
+                    <p>For every call we reject, you get paid Nano. Instantly. Cash-out at any time.</p>
+                  </div>
+                </div>
+                <div className="value-row">
+                  <div className="icon-container">
+                    <FaRobot />
+                  </div>
+                  <div className="text-container">
+                    <h4>End AI Scams</h4>
+                    <p>Scammers depend on you answering your phone. As long as your phone number works, you will get scam calls. We can stop that.</p>
+                  </div>
+                </div>
+                <div className="value-row">
+                  <div className="icon-container">
+                    <FaUserShield />
+                  </div>
+                  <div className="text-container">
+                    {/* /want to change this up a bit to focus on sanity / mental wellness / uniqueness */}
+                    <h4>Your mind. Your time.</h4>
+                    <p>Your data is valuable. We make sure you get paid for your data and time.</p>
+                  </div>
+                </div>
+              </div>
+              {/**/}
+              <p>
+                108667 payments have been made for rejected calls so far. What are you waiting for? Download the app and get paid! See these payments happening
+                in real-time.{" "}
+              </p>
+            </div>
+            <div className="logo-container">
+              <div className="bottom-logo">
+                <img src={data.standingKarmaCallPost.publicURL} className={"hero-index-image"} alt="A simple app that pays you to block scam calls." />\
+                <GatsbyImage image={increaseCustomerResponse} alt="Increase Customer Engagement" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="AppText">
+          <div className="social-media-container">
+            <div className="text-block-left">
+              <h2 className="text-wrapper-5">
+                Are your customers getting <span className="underline-red">phished</span>?
               </h2>
               <p>
                 {" "}
-                Stop scams with refundable deposits. Our tech asks unknown
-                senders to risk losing money to your audience before they can
-                reach them. Click the images to learn more.
+                Stop scams with refundable deposits. Our tech asks unknown senders to risk losing money to your audience before they can reach them. Click the
+                images to learn more.
               </p>
             </div>
             <div className="logo-container">
               <div className="top-logos-left">
-                <Link
-                  to="/fyncom-filters-email-edition"
-                  className="index-links"
-                >
-                  <GatsbyImage
-                    image={filterLogo}
-                    alt="block bad emails automatically & get paid."
-                  />
+                <Link to="/fyncom-filters-email-edition" className="index-links">
+                  <GatsbyImage image={filterLogo} alt="block bad emails automatically & get paid." />
                 </Link>
                 <a href="https://www.karmacall.com" className="index-links">
-                  <GatsbyImage
-                    image={karmacallLogo}
-                    alt="Get paid to block scam calls!"
-                  />
+                  <GatsbyImage image={karmacallLogo} alt="Get paid to block scam calls!" />
                 </a>
               </div>
             </div>
@@ -247,20 +269,15 @@ const BlockSpamEarnCash = () => {
               </div>
             </div>
             <div className="text-block">
-              <h2 className="text-wrapper-2">
-                Too many <span className="underline">abusive DMs</span> in your
-                communities?
+              <h2 className="text-wrapper-5">
+                Too many <span className="underline-red">abusive DMs</span> in your communities?
               </h2>
               <p>
                 {" "}
-                Your community loves making connections, but not with scammers
-                &amp; imposters. Help mods by adding FynCom&#39;s Direct Message
-                tech into your Discord, Telegram, or other chat-based community.
-                Bonus - your users make $$ for every blocked spam DM!{" "}
+                Your community loves making connections, but not with scammers &amp; imposters. Help mods by adding FynCom&#39;s Direct Message tech into your
+                Discord, Telegram, or other chat-based community. Bonus - your users make $$ for every blocked spam DM!{" "}
                 <span className="span">
-                  <a href="mailto:support@fyncom.com?subject=FynCom DMs">
-                    Contact us
-                  </a>
+                  <a href="mailto:support@fyncom.com?subject=FynCom DMs">Contact us</a>
                 </span>
               </p>
             </div>
@@ -268,53 +285,47 @@ const BlockSpamEarnCash = () => {
         </div>
 
         <section className="why-fyncom">
-          <h2 className="text-wrapper-2">Why FynCom?</h2>
+          <h2 className="text-wrapper-5">Why FynCom?</h2>
           <p>
-            Our company is built around refundable deposits as a tool to create
-            trust between unknown parties in digital communications. Protect
-            yourself from unwanted communications & get the power to put a
-            monetary value to your time & data. <br />
+            Our company is built around refundable deposits as a tool to create trust between unknown parties in digital communications. Protect yourself from
+            unwanted communications & get the power to put a monetary value to your time & data. <br />
             <Link to="/white-paper-original-scam-calls">Read More</Link>
           </p>
         </section>
 
-        <div className="AppText">
-          <div className="social-media-container">
-            <div className="text-block-left">
-              <h2 className="text-wrapper-2">
-                A scam blocking tool for some...
-              </h2>
-              <p>
-                <p2 className="underline-red">
-                  ...and a rewards engine for all.
-                </p2>
-                FynCom creates trust in online communications with its
-                Refundable Deposits API. Any platform with communication between
-                users can give cash-back to users for blocking scams. Bring a
-                modern approach to cybersecurity that your customers will love &
-                your revenue teams will love more.
-              </p>
-            </div>
-            <div className="logo-container">
-              <div className="bottom-logo">
-                <GatsbyImage
-                  image={increaseCustomerResponse}
-                  alt="Increase Customer Engagement"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        {/*<div className="AppText">*/}
+        {/*  <div className="social-media-container">*/}
+        {/*    <div className="text-block-left">*/}
+        {/*      <h2 className="text-wrapper-5">*/}
+        {/*        A scam blocking tool for some...*/}
+        {/*      </h2>*/}
+        {/*      <p>*/}
+        {/*        <p2 className="underline-red">*/}
+        {/*          ...and a rewards engine for all.*/}
+        {/*        </p2>*/}
+        {/*        FynCom creates trust in online communications with its*/}
+        {/*        Refundable Deposits API. Any platform with communication between*/}
+        {/*        users can give cash-back to users for blocking scams. Bring a*/}
+        {/*        modern approach to cybersecurity that your customers will love &*/}
+        {/*        your revenue teams will love more.*/}
+        {/*      </p>*/}
+        {/*    </div>*/}
+        {/*    <div className="logo-container">*/}
+        {/*      <div className="bottom-logo">*/}
+        {/*        <GatsbyImage*/}
+        {/*          image={increaseCustomerResponse}*/}
+        {/*          alt="Increase Customer Engagement"*/}
+        {/*        />*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
 
         <section className="why-fyncom">
-          <h2 className="underline">
-            Give the Gift of Cash-Back for Scam Blocking
-          </h2>
+          <h2 className="underline">Give the Gift of Cash-Back for Scam Blocking</h2>
           <p>
-            Our company is built around refundable deposits as a tool to create
-            trust between unknown parties in digital communications. Protect
-            yourself from unwanted communications & get the power to put a
-            monetary value to your time & data. <br />
+            Our company is built around refundable deposits as a tool to create trust between unknown parties in digital communications. Protect yourself from
+            unwanted communications & get the power to put a monetary value to your time & data. <br />
             <a href="https://calendly.com/adrian-fyncom/30min">
               <button className="learn-more-btn">Sieze The Day!</button>
             </a>
