6e85258fe9ae5d0373e732b8350aac3fd9d88ed3
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-11-25 18:21:48 -0800

    setting up referral and login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,65 +1,71 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import "../components/sales-and-marketing-use-cases.css"
 import Seo from "../components/seo"
-import { GatsbyImage } from "gatsby-plugin-image"
-import { useCombinedQuery } from "../components/useCombinedQuery"
+import { useLocation } from "@reach/router"
 
 const Login = () => {
-  const { sendgrid, slicktext, zapier, salesHeroImage, increaseBookings, accelerateDeals, minMax } = useCombinedQuery()
+  const [countryCode, setCountryCode] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const location = useLocation()
+
+  useEffect(() => {
+    // Get calling code or other actions on component mount
+  }, [])
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    // Handle your form submission logic here
+  }
+
   return (
-    <div className="sales-use-cases">
-      <Seo
-        title="Sales Use Cases"
-        description="Getting your sales team to hit their targets is hard. FynCom helps you close more deals by using rewards at key stages of your sales process."
-      />
+    <div className="Referral">
+      <Seo title="Login KarmaCall" description="A simple login page to let you manage your account" />
       <Header />
-      <div className="hero-section">
-        <GatsbyImage image={salesHeroImage} alt="use rewards at each touchpoint to keep leads moving down through the customer journey" />
-        <div className="hero-content">
-          <h1>Accelerate Sales</h1>
-          <p>Close more deals with customers by using interactive rewards at key stages of your sales process</p>
-          <a href="https://calendly.com/adrian-fyncom/30min">
-            <button className="demo-button">Request Demo</button>
-          </a>
+      <section>
+        <div id="Phone Number Entry" className="network">
+          <div className="container">
+            <div className="col-md-2"></div>
+            <form method="get" id="phoneNumberInput" onSubmit={handleSubmit}>
+              <div>
+                <p>
+                  <label>Country Code:</label>
+                  <select name="countryCodes" id="countryCodes" value={countryCode} onChange={e => setCountryCode(e.target.value)}>
+                    {/* Options here */}
+                  </select>
+                </p>
+                <p style={{ alignContent: "center" }}>
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    id="phoneNumber"
+                    placeholder="Enter Phone Number"
+                    style={{ maxWidth: "100%" }}
+                    className="form-control"
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
+                  />
+                </p>
+              </div>
+              <div className="input-group-btn">
+                <p style={{ alignContent: "center" }}>
+                  <span className="input-group-btn">
+                    <button type="submit" style={{ backgroundColor: "#54b07c", maxWidth: "100%" }} className="btn btn-info">
+                      Confirm Phone Number
+                    </button>
+                  </span>
+                </p>
+              </div>
+            </form>
+            <h3 style={{ alignContent: "center" }}>
+              <a style={{ WebkitTextFillColor: "black" }} href="/login-email">
+                Click here to login with email
+              </a>
+            </h3>
+            <div className="col-md-2"></div>
+          </div>
         </div>
-      </div>
-
-      <div className="use-cases-sales-marketing-container">
-        <div className="use-case">
-          <GatsbyImage image={increaseBookings} alt="Increase bookings with rewards before and after a meeting" />
-          <h2>Increase Bookings</h2>
-          <p>Incentivize your prospective buyers to book time with you by offering rewards for scheduling meetings</p>
-        </div>
-        <div className="use-case">
-          <GatsbyImage image={accelerateDeals} alt="Accelerate Deals with customer journey rewards" />
-          <h2>Accelerate Deals</h2>
-          <p>Move prospective clients through your sales journey by providing rewards for reaching the next stage</p>
-        </div>
-        <div className="use-case">
-          <GatsbyImage image={minMax} alt="Min / max the value you give based on the revenue you generate" />
-          <h2>Min/Max Gifts/Value</h2>
-          <p>Only pay for rewards when your sales target moves along to the next stage of your sales journey</p>
-        </div>
-      </div>
-
-      <div className="integrations-section">
-        <h2 className="background-stripe">Popular Integrations</h2>
-        <div className="logos-container">
-          <GatsbyImage image={sendgrid} alt="SendGrid" className={"logo"} />
-          <GatsbyImage image={slicktext} alt="SlickText" className={"logo"} />
-          <GatsbyImage image={zapier} alt="Zapier" className={"logo"} />
-        </div>
-      </div>
-
-      <div className="call-to-action">
-        <h2>Accelerate sales pipeline & hit revenue targets by using action-oriented rewards to guide prospects through your funnels</h2>
-        <a href="https://calendly.com/adrian-fyncom/30min" className="cta-button">
-          Request Demo
-        </a>
-      </div>
-
+      </section>
       <Footer />
     </div>
   )
